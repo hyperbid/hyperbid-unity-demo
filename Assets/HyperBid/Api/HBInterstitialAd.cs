49364121e333100f9dd3325cb67aadb6c9a2703c
Dorin <dorin@gameanalytics.com>
2021-09-01 11:50:22 +0300

    added accessors for the interstitial ad events

--- Assets/HyperBid/Api/HBInterstitialAd.cs
+++ Assets/HyperBid/Api/HBInterstitialAd.cs
@@ -14,6 +14,34 @@ namespace HyperBid.Api
 		private static readonly HBInterstitialAd instance = new HBInterstitialAd();
 		private IHBInterstitialAdClient client;
 
+        
+        public event EventHandler<HBAdEventArgs> onAdLoad 
+            {add {client.onAdLoad += value;} remove {client.onAdLoad -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdLoadFailed 
+            {add {client.onAdLoadFailed += value;} remove {client.onAdLoadFailed -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdShow 
+            {add {client.onAdShow += value;} remove {client.onAdShow -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdShowFailed 
+            {add {client.onAdShowFailed += value;} remove {client.onAdShowFailed -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdClose 
+            {add {client.onAdClose += value;} remove {client.onAdClose -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdClick 
+            {add {client.onAdClick += value;} remove {client.onAdClick -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdPlayVideo 
+            {add {client.onAdPlayVideo += value;} remove {client.onAdPlayVideo -= value;}}
+
+        public event EventHandler<HBAdEventArgs> onAdPlayVideoFailed 
+            {add {client.onAdPlayVideoFailed += value;} remove {client.onAdPlayVideoFailed -= value;}}
+            
+        public event EventHandler<HBAdEventArgs> onAdEndVideo 
+            {add {client.onAdEndVideo += value;} remove {client.onAdEndVideo -= value;}}
+
 		private HBInterstitialAd()
 		{
             client = GetHBInterstitialAdClient();
@@ -30,11 +58,6 @@ namespace HyperBid.Api
 		public void loadInterstitialAd(string placementId, Dictionary<string,string> pairs)
         {
             client.loadInterstitialAd(placementId, Json.Serialize(pairs));
-        }
-
-		public void setListener(HBInterstitialAdListener listener)
-        {
-            client.setListener(listener);
         }
 
         public bool hasInterstitialAdReady(string placementId)
