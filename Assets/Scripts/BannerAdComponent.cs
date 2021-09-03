d45309d63ec9abe326107063b1df952812e940f7
Dorin <dorin@gameanalytics.com>
2021-09-03 15:08:40 +0300

    Added more sample listeners to the banner sample

--- Assets/Scripts/BannerAdComponent.cs
+++ Assets/Scripts/BannerAdComponent.cs
@@ -11,20 +11,54 @@ public class BannerAdComponent : MonoBehaviour
 {
     static readonly protected string _placementId = PlacementId.AD_BANNER;
     
-    protected int _width;
-    protected int _height;
-    
-    protected void OnAdLoadFail(object sender, HBAdEventArgs args) {
+    protected int  _width;
+    protected int  _height;
+    protected bool _isHidden;
+
+
+    // event callback for when the ad is loaded
+    protected void OnAdLoad(object sender, HBAdEventArgs args)
+    {
+        Debug.Log("BannerAd - OnAdLoad");
         var txt = GameObject.Find("detailsHyperTxt/Text").GetComponent<Text>();
-        txt.text = args.errorMessage;
+        txt.text = "Banner ad has been loaded succesfully";
+    }
+
+    // event callback if the ad has failed to load
+    protected void OnAdLoadFail(object sender, HBAdEventArgs args) {
+        Debug.Log("BannerAd - OnAdLoadFail");
+        var txt = GameObject.Find("detailsHyperTxt/Text").GetComponent<Text>();
+        txt.text = "Failed to load banner ad: " + args.errorMessage;
+    }
+
+    // event callback if the ad has been clicked
+    protected void OnAdClicked(object sender, HBAdEventArgs args) {
+        Debug.Log("Banner Ad - OnAdClicked");
+        var txt = GameObject.Find("detailsHyperTxt/Text").GetComponent<Text>();
+        txt.text = "Banner Ad has been clicked";
+    }
+
+    // event callback if the ad has been closed
+    protected void OnAdClosed(object sender, HBAdEventArgs args) {
+        Debug.Log("Banner Ad - OnAdClosed");
+        var txt = GameObject.Find("detailsHyperTxt/Text").GetComponent<Text>();
+        txt.text = "Banner Ad has been closed";
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        _width  = Screen.width;
+        //var adPanel = GameObject.Find("").GetComponent<RectTransform>();
+
+        // set the dimensions of the banner
+        _width  = Screen.width;//(int)adPanel.sizeDelta.x;
         _height = 100;
-        HBBannerAd.Instance.events.onAdLoadFailedEvent += OnAdLoadFail;
+
+        // register the needed callbacks
+        HBBannerAd.Instance.events.onAdLoadEvent        += OnAdLoad;
+        HBBannerAd.Instance.events.onAdLoadFailedEvent  += OnAdLoadFail;
+        HBBannerAd.Instance.events.onAdClickEvent       += OnAdClicked;
+        HBBannerAd.Instance.events.onAdCloseEvent       += OnAdClosed;
     }
 
     // Update is called once per frame
@@ -43,20 +77,24 @@ public class BannerAdComponent : MonoBehaviour
         HBBannerAd.Instance.loadBannerAd(_placementId, jsonmap);
     }
 
+    // displays if the ad is ready
     public void IsAdReady() {
         var detailsText = GameObject.Find("detailsHyperTxt/Text").GetComponent<Text>();
         detailsText.text = HBBannerAd.Instance.checkAdStatus(_placementId);
     }
 
+    // shows the ad on the screen
     public void ShowAd() {
         HBBannerAd.Instance.showBannerAd(_placementId, HBBannerAdLoadingExtra.kHBBannerAdShowingPisitionBottom);
     }
 
-    public void HideAd() {
-        HBBannerAd.Instance.hideBannerAd(_placementId);
+    // closes the ad
+    public void CloseAd() {
+        HBBannerAd.Instance.cleanBannerAd(_placementId);
     }
 
-    void ClearAd() {
-
+    // hides the banner
+    void HideAd() {
+        HBBannerAd.Instance.hideBannerAd(_placementId);
     }
 }
